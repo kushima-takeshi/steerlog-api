3e13c42ab2296e6f9aa1d106df8d52bfcf782616
user <wushijiudao@gmail.com>
2026-05-30 13:48:00 +0900

    chore: bootstrap Spring Boot project with PostgreSQL and Flyway

---
+++ src/main/java/com/steerlog/SteerlogApiApplication.java
@@ -0,0 +1,12 @@
+package com.steerlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteerlogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteerlogApiApplication.class, args);
+    }
+}
